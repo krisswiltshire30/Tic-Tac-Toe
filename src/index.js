187b9a8ed29feb36dd808e3facfe22e273763b61
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-09-08 18:18:05 +0100

    Click in box adds X - Board styleing changed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: null
+    }
+    
+  }
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button 
+      className="square" 
+      onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
-    const status = "Next player: X";
+    const status = 'Next player: X';
 
     return (
       <div>
@@ -57,4 +70,4 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<Game />, document.getElementById('root'));
